1657a4815a09780df9b314d92805baf269cb71bc
beio <beioo@live.com>
2016-09-05 21:56:24 +0800

    del url tinymce

--- mysite/urls.py
+++ mysite/urls.py
@@ -22,5 +22,5 @@ urlpatterns = [
     url(r'', include('blog.urls')),
     url(r'^accounts/login/$', auth_views.login, name='login'),
     url(r'^accounts/logout/$', auth_views.logout, name='logout'),
-    url(r'^tinymce/', include('tinymce.urls')),
+
 ]
