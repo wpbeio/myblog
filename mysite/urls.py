e146f4661896055295576601192c554dfe7c442f
beio <beioo@live.com>
2018-01-07 14:49:10 +0800

    test git

--- mysite/urls.py
+++ mysite/urls.py
@@ -17,7 +17,8 @@ from django.conf.urls import url, include
 from django.contrib import admin
 from django.contrib.auth import views as auth_views
 from django.conf.urls.static import static
-from django.conf import settings
+from django.conf import settings 
+
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
